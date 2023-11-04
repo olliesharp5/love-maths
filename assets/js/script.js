5038a863b0933a2d61519a9e2f395bd5a277f981
olliesharp5 <>
2023-11-04 15:16:55 +0000

    Add code to generate 2 random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,8 +16,13 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 })
 
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 function runGame() {
-
+    let num1 = Math.floor(Math.random() * 25) +1; // generates random whole integer from 1 to 25 excluding zero
+    let num2= Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
